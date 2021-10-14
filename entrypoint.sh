43795fa0b3b369f42ac062ba035082382ceced4a
balli1187 <whistle.balli@web.de>
2021-10-14 10:39:41 +0200

    Update entrypoint.sh
    fixed typos

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,19 +13,19 @@ chmod 664 /var/log/dovecot/dovecot.log
 for USER in $(ls -1 /home); do
   echo "User '$USER':"
   if ! id -u "$USER" >/dev/null 2>&1; then
-	# respect hosts permissions by cloning uid, gid and group into container
-	host_uid=$(stat -c '%u' /home/$USER)
-	host_gi=$(stat -c '%g' /home/$USER)
+    # respect hosts permissions by cloning uid:gid into container
+    host_uid=$(stat -c '%u' /home/$USER)
+    host_gid=$(stat -c '%g' /home/$USER)
     # create user with default password
     useradd --uid=$host_uid --gid=$host_gid --groups=users --no-create-home --shell='/bin/true' "$USER"
     echo -e "$DEFAULT_PASSWD\n$DEFAULT_PASSWD\n" | passwd "$USER"
+    # create subfolders if necessary
+    mkdir -p /home/$USER/{Maildir,sieve}
     chown -R "$host_uid:$host_gid" "/home/$USER"
-	# create subfolders if necessary
-	mkdir -p /home/$USER/{Maildir,sieve}
     chmod 700 /home/$USER/{Maildir,sieve,.getmail} || true
   fi
   for RC in $(ls -1 /home/$USER/.getmail/getmailrc-*); do
-    echo "- $RC"
+    echo "\t- $RC"
     # fix log permissions
     LOG="/var/log/getmail/${RC##*/getmailrc-}.log"
     touch "$LOG"
